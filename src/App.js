978e228d5616a5929451a6c208e0e7f43883b549
Dyadya <farhod.jonikulovich@gmail.com>
2021-11-25 12:40:25 +0300

    add task

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     { id: 1, name: "send message", completed: false },
     { id: 2, name: "fix auto", completed: false },
     { id: 3, name: "but something", completed: false },
+    { id: 4, name: "Go home", completed: false },
   ]);
 
   function deleteTodoItem(id) {
