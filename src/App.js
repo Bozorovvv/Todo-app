354b9dd2865f418c8c64f134965b087515d4f86d
Dyadya <farhod.jonikulovich@gmail.com>
2021-11-25 12:36:24 +0300

    1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Input from "./components/Input";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [todos] = useState([
+  const [todos, setTodos] = useState([
     { id: 1, name: "send message", completed: false },
     { id: 2, name: "fix auto", completed: false },
     { id: 3, name: "but something", completed: false },
